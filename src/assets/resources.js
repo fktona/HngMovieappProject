459d0d91f60f748d0e6a43197f24280b5a3d5463
fktona <adetona.fk@gmail.com>
2023-09-14 12:19:47 +0100

    environment

--- src/assets/resources.js
+++ src/assets/resources.js
@@ -41,18 +41,26 @@
  
 // };
 
+import config from '../../config.json';
+
+const apiKey = config.apiKey;
+const bearerToken = config.bearerToken;
+
+// Use apiKey and bearerToken in your API requests
+
+
 
 
 const options = {
   method: 'GET',
   headers: {
     accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiYmE3Y2MyMTBlMTY5NTFhYmFhNjg5ZjVlYWYzMmQyMiIsInN1YiI6IjY0ZmU1ZDM0ZTBjYTdmMDBlYzhjMDY4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4HftPZ_p_hwAPLIBiHtgGtzbs2u_04elc4xu2dpXLU0'
+    Authorization: `Bearer ${bearerToken}`
   }
 };
 
 export const resources = async (resourceName, queryParams = {}) => {
-  const apiKey = 'bba7cc210e16951abaa689f5eaf32d22';
+  
 
   const endpoint = `https://api.themoviedb.org/3/${resourceName}?${new URLSearchParams(queryParams)}&api_key=${apiKey}`;
 
